0b48cfa69499e2f5daf54169aa6f799f8f85d118
dih <openttd@dihedral.de>
2011-02-12 13:20:26 +0100

    Cleanup: remove unneeded imports

--- password/src/main/java/org/openttdcoop/dev/berries/password/Password.java
+++ password/src/main/java/org/openttdcoop/dev/berries/password/Password.java
@@ -1,13 +1,9 @@
 package org.openttdcoop.dev.berries.password;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.LineNumberReader;
-import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
